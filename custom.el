cb2e68e13ad6c9572133c9998314905ac24b62f0
Max Miliano <milianordelcanto@gmail.com>
2026-03-28 08:50:13 -0300

    feat: completion

--- custom.el
+++ custom.el
@@ -5,8 +5,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(autothemer command-log-mode dirvish meow multiple-cursors nerd-icons
-		org-modern)))
+   '(autothemer command-log-mode dirvish marginalia meow multiple-cursors
+		nerd-icons nerd-icons-completion orderless org-modern
+		vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -165,3 +165,51 @@
   :hook (org-mode . org-modern-mode)
   :config
   (setq org-modern-star '("◉" "○" "◈" "◇" "✳")))
+
+
+(use-package vertico
+  :ensure t
+  :bind (:map vertico-map
+              ("TAB" . vertico-next)
+              ("<backtab>" . vertico-previous)
+              ("C-M-v" . vertico-scroll-down)
+              ("<right>" . vertico-insert))
+  :init
+  (vertico-mode))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :ensure t
+  :custom
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-category-defaults nil) ;; Disable defaults, use our settings
+  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
+
+(use-package marginalia
+  :ensure t
+  :after vertico
+  :config
+  (marginalia-mode))
+
+(use-package nerd-icons-completion
+  :ensure t
+  :after marginalia
+  :config
+  (nerd-icons-completion-mode)
+  :hook
+  (marginalia-mode . nerd-icons-completion-marginalia-setup))
+
+(use-package nerd-icons-completion
+  :after marginalia
+  :config
+  (nerd-icons-completion-mode)
+  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
