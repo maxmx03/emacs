da89ff02481265256015202623fd993f3b822bec
Max Miliano <milianordelcanto@gmail.com>
2026-03-28 17:19:34 -0300

    feat: dashboard

--- custom.el
+++ custom.el
@@ -4,7 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(autothemer command-log-mode corfu dashboard dirvish marginalia meow
+		multiple-cursors nerd-icons-completion
+		nerd-icons-corfu orderless org-modern vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -59,3 +59,30 @@
 (require 'vertico-setup)
 (require 'corfu-setup)
 (require 'treesitter-setup)
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  ;; Personalização para o seu workflow:
+  (setq dashboard-items '((recents  . 5)   ;; 5 arquivos mais recentes
+                          (projects . 5)   ;; Seus últimos projetos C++
+                          (agenda   . 5))) ;; O que tem pra hoje no Org-mode
+  
+  ;; Estética (já que você usa nerd-icons)
+  (setq dashboard-display-icons-p t) 
+  (setq dashboard-icon-type 'nerd-icons)
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-startup-banner '(1 2 3))
+  (setq dashboard-banner-logo-title "Success is the sum of small efforts, repeated day in and day out.")
+  (setq dashboard-footer-messages
+      '(
+        "Emacs: An operating system that happens to have a text editor."
+        "Opening Emacs is the first step. Closing it is the final boss."
+        "Configuring Emacs is not procrastination, it's 'Workflow Engineering'."
+        "Your Org-mode habits are watching you. Go study Math!"
+	"E.M.A.C.S. - Eight Megabytes And Constantly Swapping (circa 1985)."
+        "Emacs is not a religion. It's just the only true way to live."
+	"Stop staring at this dashboard and M-x compile your C++ code!"))
+  (setq dashboard-set-navigator t))
