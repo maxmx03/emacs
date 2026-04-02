2aa91b3d9ebeaf5e606ca64b7b977f5e3f21d73d
Max Miliano <milianordelcanto@gmail.com>
2026-04-02 16:43:30 -0300

    feat: treesitter,corfu,lsp removed

--- modules/projectile-setup.el
+++ modules/projectile-setup.el
@@ -7,6 +7,6 @@
   ;; (projectile-auto-discover nil) ;; Disable auto search for better startup times ;; Search with a keybind
   (projectile-run-use-comint-mode t) ;; Interactive run dialog when running projects inside emacs (like giving input)
   (projectile-switch-project-action #'projectile-dired) ;; Open dired when switching to a project
-  (projectile-project-search-path '("~/projects/" "~/work/" ("~/github" . 1)))) ;; . 1 means only search the first subdirectory level for projects
+  (projectile-project-search-path '("~/.org/" ))) ;; . 1 means only search the first subdirectory level for projects
 
 (provide 'projectile-setup)
