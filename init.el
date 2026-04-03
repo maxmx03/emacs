d47430240139df3ca1838e5ca41d7b87fd453f3b
Max Miliano <milianordelcanto@gmail.com>
2026-04-03 09:06:16 -0300

    feat: eglot,treesitter,company

--- init.el
+++ init.el
@@ -24,6 +24,7 @@
   (setq mouse-wheel-progressive-speed nil)
   (setq scroll-conservatively 10)
   (setq scroll-margin 8)
+  (scroll-bar-mode 0)
   (set-face-attribute 'default nil :font "JetBrainsMono NF" :height 160))
 
 (use-package autothemer
@@ -47,6 +48,52 @@
 (require 'projectile-setup)
 (require 'consult-setup)
 (require 'dirvish-setup)
+
+(use-package eglot
+  :ensure nil
+  :hook ((c-mode . eglot-ensure)
+         (c++-mode . eglot-ensure)
+         (c-ts-mode . eglot-ensure)
+         (c++-ts-mode . eglot-ensure))
+  :config
+  (setq eglot-autostart t))
+
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode t)
+  (setq company-idle-delay 0.1
+        company-minimum-prefix-length 1))
+
+(setq treesit-language-source-alist
+      '((elisp "https://github.com/Wilfred/tree-sitter-elisp")
+        (cmake "https://github.com/uyha/tree-sitter-cmake")
+        (c "https://github.com/tree-sitter/tree-sitter-c")
+        (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+        (json "https://github.com/tree-sitter/tree-sitter-json")
+        (make "https://github.com/alemuller/tree-sitter-make")
+        (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+	(markdown "https://github.com/ikatyang/tree-sitter-markdown")))
+
+(defun start/install-treesit-grammars ()
+  "Install missing treesitter grammars"
+  (interactive)
+  (dolist (grammar treesit-language-source-alist)
+    (let ((lang (car grammar)))
+      (unless (treesit-language-available-p lang)
+        (treesit-install-language-grammar lang)))))
+
+(add-hook 'after-init-hook #'start/install-treesit-grammars)
+
+(setq major-mode-remap-alist
+      '((sh-mode . bash-ts-mod)
+        (c-mode . c-ts-mode)
+        (c++-mode . c++-ts-mode)
+	(js-json-mode . json-ts-mode)
+	(yaml-mode . yaml-ts-mode)
+	))
+
+(setq treesit-font-lock-level 4)
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
