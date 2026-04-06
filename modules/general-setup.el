944b099bf6a23bb2b8c8ad4fe7f8709c7301fb2d
Max Miliano <milianordelcanto@gmail.com>
2026-04-05 22:48:42 -0300

    refactor: general

--- modules/general-setup.el
+++ modules/general-setup.el
@@ -1,158 +1,104 @@
-(defvar-keymap my-expand-mode-map
-  :doc "Keybinds for Expand Mode."
-  "y" 'kill-ring-save
-  "d" 'kill-region
-  "u" 'upcase-region
-  "l" 'downcase-region
-  "w" 'er/mark-word
-  "q" 'er/mark-inside-quotes
-  "Q" 'er/mark-outside-quotes
-  "p" 'er/mark-inside-pairs
-  "P" 'er/mark-outside-pairs
-  "c" 'er/mark-comment
-  ;; "u" 'er/mark-url
-  ;; "d" 'er/mark-defun
-  "f" 'er/mark-method-call
-  "t" 'er/mark-inner-tag
-  "T" 'er/mark-outer-tag
-  "a" 'er/mark-html-attribute)
-
-(defvar-keymap my-normal-mode-map
-  :doc "Keybindings for Normal Mode."
-  "1" 'delete-other-windows
-  "2" 'split-window-below
-  "3" 'split-window-right
-  "0" 'beginning-of-line
-  "o" 'new-line-below
-  "O" 'new-line-above
-  "a" 'insert-mode-beginning-line
-  "A" 'insert-mode-end-line
-  "e" 'insert-mode-end-line
-  "E" 'insert-mode-beginning-line
-  "$" 'end-of-line
-  "w" 'right-word
-  "W" 'left-word
-  "y" 'kill-ring-save
-  "Y" 'copy-line
-  "u" 'undo
-  "U" 'undo-redo
-  "q" 'kill-current-buffer
-  "f" 'search-forward
-  "F" 'search-backward
-  "r" 'consult-find
-  "R" 'consult-grep
-  "b" 'consult-buffer
-  "g" 'end-of-buffer
-  "G" 'beginning-of-buffer
-  "v" 'my-expand-mode
-  "k" 'kill-line
-  "K" 'kill-sentence
-  "x" 'execute-extended-command
-  "s" 'save-buffer
-  "d" 'kill-word
-  "D" 'backward-kill-word
-  "c" 'delete-char
-  "C" 'backward-delete-char
-  "C-d" 'mc/mark-all-like-this
-  "i" 'my-exit-normal-mode
-  "\\" 'comment-line
-  "M-<up>" 'org-drag-line-backward
-  "M-<down>" 'org-drag-line-forward
-  "n" 'dired
-  "p" 'yank
-  "SPC" 'set-mark-command)
-
 (use-package expand-region :ensure t)
 (use-package multiple-cursors
   :ensure t
   :config
-  (add-hook 'multiple-cursors-mode-enabled-hook #'my-exit-normal-mode)
-  (add-hook 'multiple-cursors-mode-disabled-hook #'my-enter-normal-mode))
+  (add-hook 'multiple-cursors-mode-enabled-hook #'insert-mode)
+  (add-hook 'multiple-cursors-mode-disabled-hook #'normal-mode))
 
-(define-minor-mode my-normal-mode
-  "Command state (Neovim style)."
-  :lighter " [NORMAL]"
-  :keymap my-normal-mode-map
-  (if my-normal-mode
-      (progn
-        (my-expand-mode -1)
-        (setq cursor-type 'box)
-        (message "[NORMAL]"))
-    (unless (or my-normal-mode my-expand-mode)
-      (setq cursor-type 'bar)
-      (message "[INSERT]"))))
-
-(define-minor-mode my-expand-mode
-  "State of visual selection."
-  :lighter " [EXPAND]"
-  :keymap my-expand-mode-map
-  (if my-expand-mode
-      (progn
-        (my-normal-mode -1)
-        (setq cursor-type '(hbar . 7))
-        (message "[EXPAND]"))
-    (unless (or my-normal-mode my-expand-mode)
-      (setq cursor-type 'bar)
-      (message "[INSERT]"))))
-
-(defun my-enter-normal-mode ()
-  "Activates normal mode and clears selections."
-  (interactive)
-  (deactivate-mark)
-  (my-normal-mode 1))
-
-(defun my-exit-normal-mode ()
-  "Exit all modal modes and return to the default insertion mode."
-  (interactive)
-  (my-expand-mode -1)
-  (my-normal-mode -1)
-  (setq cursor-type 'bar)
-  (message "[INSERT]"))
-
-(defun my/smart-esc ()
-  "The maestro of navigation: always trying to bring you back to NORMAL."
-  (interactive)
-  (cond
-   ((window-minibuffer-p) (abort-recursive-edit))
-   ((or my-expand-mode (region-active-p)) (my-enter-normal-mode))
-   (t (my-enter-normal-mode))))
-
-(defun insert-mode-end-line ()
-  "Enter in insert mode at end of line"
+(defun insert-nextline ()
+  "Enter insert mode nextline"
   (interactive)
   (end-of-line)
-  (my-normal-mode -1))
+  (newline-and-indent)
+  (insert-mode))
 
-(defun insert-mode-beginning-line ()
-  "Enter in insert mode at begin of line"
+(defun insert-prevline()
+  "Enter insert mode prevline"
   (interactive)
-  (beginning-of-line)
-  (my-normal-mode -1))
-
-(defun new-line-below ()
-  "Open a new line below and enter insert mode."
-  (interactive)
-  (end-of-line)
-  (newline-and-indent)    
-  (my-exit-normal-mode))
-
-(defun new-line-above ()
-  "Open a new line above and enter insert mode."
-  (interactive)
-  (read-only-mode -1)
   (beginning-of-line)
   (open-line 1)
-  (my-exit-normal-mode))
-
-(global-set-key (kbd "<escape>") #'my/smart-esc)
-
-(dolist (hook '(prog-mode-hook org-mode-hook text-mode-hook))
-  (add-hook hook #'my-normal-mode))
+  (insert-mode))
 
 (defun copy-line ()
   (interactive)
   (save-excursion
     (kill-ring-save (line-beginning-position) (line-end-position))))
+
+(defvar-keymap normal-mode-map
+  :doc "keybinds for normal mode"
+  "1" 'delete-other-windows
+  "2" 'split-window-below
+  "3" 'split-window-right
+  "'" 'other-window
+  "0" 'delete-window
+  "@" 'set-mark-command
+  "a" 'move-beginning-of-line
+  "A" 'backward-sentence
+  "b" 'backward-word
+  "d" 'delete-char
+  "e" 'move-end-of-line
+  "E" 'forward-sentence
+  "f" 'forward-word
+  "g" 'end-of-buffer
+  "G" 'beginning-of-buffer
+  "j" 'electric-newline-and-maybe-indent
+  "k" 'kill-whole-line
+  "l" 'recenter-top-bottom
+  "n" 'next-line
+  "o" 'insert-nextline
+  "O" 'insert-prevline
+  "q" 'quoted-insert
+  "r" 'isearch-backward
+  "s" 'isearch-forward
+  "i" 'insert-mode
+  "u" 'redo
+  "/" 'undo
+  "w" 'kill-region
+  "y" 'kill-ring-save
+  "Y" 'copy-line
+  "p" 'yank
+  "\\" 'comment-line
+  "x" 'execute-extended-command
+  "c" 'kill-ring-save
+  "C-d" 'mc/mark-next-like-this
+  "m w" 'mark-word
+  "m p" 'mark-paragraph
+  "m q" 'er/mark-inside-quotes
+  "m (" 'er/mark-inside-pairs
+  "m d" 'mark-defun
+  "m m" 'er/mark-method-call
+  "m t" 'er/mark-inner-tag
+  "SPC" 'set-mark-command)
+
+(define-minor-mode insert-minor-mode
+  "Enter insert mode, user can edit file"
+  :lighter " [INSERT]"
+  :keymap nil
+  (if insert-minor-mode
+      (setq cursor-type 'bar)
+    (setq cursor-type 'box)))
+
+(define-minor-mode normal-minor-mode
+  "Command state"
+  :lighter " [NORMAL]"
+  :keymap normal-mode-map
+  (if normal-minor-mode
+      (setq cursor-type 'box)
+    (setq cursor-type 'bar)))
+
+(defun insert-mode ()
+  "Enter insert mode"
+  (interactive)
+  (insert-minor-mode 1)
+  (normal-minor-mode -1))
+
+(defun my/smart-esc ()
+  "Return to NORMAL mode"
+  (interactive)
+  (normal-minor-mode 1)
+  (insert-minor-mode -1))
+
+(global-set-key (kbd "<escape>") #'my/smart-esc)
 
 (defvar-keymap my-org-roam-map
   :doc "Atalhos para o Zettelkasten."
@@ -162,6 +108,9 @@
   "g" 'org-roam-ui-mode
   "t" 'org-capture)
 
-(keymap-set my-normal-mode-map ";" my-org-roam-map)
+(keymap-set normal-mode-map ";" my-org-roam-map)
+
+(dolist (hook '(prog-mode-hook org-mode-hook text-mode-hook))
+  (add-hook hook #'normal-minor-mode))
 
 (provide 'general-setup)
